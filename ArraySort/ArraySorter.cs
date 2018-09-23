b90b556f01bd42ea2a5e8d16fa45c204e9a4dda2
newLana <newLana@users.noreply.github.com>
2018-09-23 15:28:39 +0300

    Add project files.

---
+++ ArraySort/ArraySorter.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySort
+{
+    public static class ArraySorter
+    {
+        #region MergeSorting
+        public static void MergeSorting(int[] a, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = (l + r) / 2;
+                MergeSorting(a, l, m);
+                MergeSorting(a, m + 1, r);
+
+                //слияние
+                int[] buf = new int[a.Length];
+                Array.Copy(a, buf, buf.Length);
+
+                int j = l;
+                int k = m + 1;
+                for (int i = l; i <= r; i++)
+                {
+                    if (j > m)
+                    {
+                        a[i] = buf[k];
+                        k++;
+                    }
+                    else if (k > r)
+                    {
+                        a[i] = buf[j];
+                        j++;
+                    }
+                    else if (buf[j] < buf[k])
+                    {
+                        a[i] = buf[j];
+                        j++;
+                    }
+                    else
+                    {
+                        a[i] = buf[k];
+                        k++;
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region QuickSort
+        public static void QuickSorting(int[] a, int l, int r)
+        {
+            if (l >= r)
+                return;
+
+            Random rand = new Random();
+            int index = rand.Next(l, r);
+            Swap(ref a[index], ref a[l]);
+
+            int m = Partition(a, l, r);
+            QuickSorting(a, l, m - 1);
+            QuickSorting(a, m + 1, r);
+        }
+
+        public static int Partition(int[] a, int l, int r)
+        {
+            int i = l;
+            int j = r + 1;
+
+            while (true)
+            {
+                do
+                {
+                    j--;
+                } while (a[i] < a[j]);
+                if (i >= j)
+                    break;
+                Swap(ref a[i], ref a[j]);
+
+                do
+                {
+                    i++;
+                } while (a[i] < a[j]);
+                if (i >= j)
+                    break;
+                Swap(ref a[i], ref a[j]);
+            }
+            return j;
+        }
+
+        public static void Swap(ref int first, ref int second)
+        {
+            int buf;
+            buf = first;
+            first = second;
+            second = buf;
+        }
+        #endregion
+
+        #region InsertionSort
+        public static void InsertionSorting(int[] a)
+        {
+            for (int i = 1; i < a.Length; i++)
+            {
+                int x = a[i];
+                int j = i;
+                while (j > 0 && a[j - 1] > x)
+                {
+                    a[j] = a[j - 1];
+                    j--;
+                }
+                a[j] = x;
+            }
+        }
+        #endregion   
+    }
+}
---
+++ ArraySort/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ArraySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter array");
+            string s = Console.ReadLine();
+            var charArray = s.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            int[] intArray = new int[charArray.Length];
+            
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (!int.TryParse(charArray[i], out intArray[i]))
+                {
+                    intArray[i] = 0;
+                }
+            }
+
+            var sw = new Stopwatch();
+            var quickSortArray = new int[intArray.Length];
+            Array.Copy(intArray, quickSortArray, intArray.Length);
+            sw.Start();
+
+            ArraySorter.QuickSorting(quickSortArray, 0, quickSortArray.Length - 1);
+            sw.Stop();
+            Console.WriteLine("QuickSort");
+            foreach (var item in quickSortArray)
+            {
+                Console.Write("\t{0}", item);
+            }
+            Console.WriteLine("\nВремя сортировки: {0}", sw.Elapsed);
+            Console.WriteLine();
+
+
+            var insertionSortArray = new int[intArray.Length];
+            Array.Copy(intArray, insertionSortArray, intArray.Length);
+            sw.Reset();
+            sw.Start();
+            ArraySorter.InsertionSorting(insertionSortArray);
+            sw.Stop();
+            Console.WriteLine("InsertionSort");
+            foreach (var item in insertionSortArray)
+            {
+                Console.Write("\t{0}", item);
+            }
+            Console.WriteLine("\nВремя сортировки: {0}", sw.Elapsed);
+            Console.WriteLine();
+
+            var mergeSortArray = new int[intArray.Length];
+            Array.Copy(intArray, mergeSortArray, intArray.Length);
+            sw.Reset();
+            sw.Start();
+            ArraySorter.MergeSorting(mergeSortArray, 0, mergeSortArray.Length-1);
+            sw.Stop();
+            Console.WriteLine("MergeSort");
+            foreach (var item in mergeSortArray)
+            {
+                Console.Write("\t{0}", item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Время сортировки: {0}", sw.Elapsed);
+            Console.ReadKey();
+        }   
+    }
+}
